18d89ad41c1fd2bec6cfba399d4368fae749cb83
jinsoo <binary01@kakao.com>
2024-10-18 18:47:48 +0900

    build: esm만 지원하도록 수정

--- packages/notion-to-utils/tsup.config.ts
+++ packages/notion-to-utils/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   target: 'esnext',
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   splitting: true,
   treeshake: true,
   clean: true,
