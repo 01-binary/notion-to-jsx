9eac472f0523ed1887fdb3ad972abf5ead7ddec2
jinsoo <binary01@kakao.com>
2024-10-25 17:08:37 +0900

    refactor: client 메소드 분리

---
+++ packages/notion-to-utils/src/client/getPageProperties.ts
@@ -0,0 +1,37 @@
+import { Client } from '@notionhq/client';
+import { isPageObjectResponse } from '../page/isPageObjectResponse';
+
+/**
+ * Retrieves properties of a Notion page.
+ *
+ * @param {Client} client - The Notion client instance.
+ * @param {string} pageId - The ID of the Notion page.
+ * @param {string[]} [keys=[]] - Optional array of property keys to filter. If empty, all properties are returned.
+ * @returns {Promise<PageObjectResponse['properties'] | undefined>} The requested page properties or undefined if the page is not found or is not a PageObjectResponse.
+ */
+export async function getPageProperties(
+  client: Client,
+  pageId: string,
+  keys: string[] = []
+) {
+  const page = await client.pages.retrieve({
+    page_id: pageId,
+  });
+
+  if (!isPageObjectResponse(page)) return;
+  const { properties } = page;
+
+  if (keys.length === 0) {
+    return properties;
+  }
+
+  const filteredProperties: Record<string, any> = {};
+
+  keys.forEach((key) => {
+    if (key in properties) {
+      filteredProperties[key] = properties[key];
+    }
+  });
+
+  return filteredProperties;
+}
--- packages/notion-to-utils/src/client/index.ts
+++ packages/notion-to-utils/src/client/index.ts
@@ -1,57 +1,17 @@
 import { Client as NotionClient } from '@notionhq/client';
-
 import type { ClientOptions } from '@notionhq/client/build/src/Client';
-import { isPageObjectResponse } from '../page/isPageObjectResponse';
-import { isPropertyFileType } from '../file/isPropertyFileType';
+import { getPageProperties as getPagePropertiesFunc } from './getPageProperties';
+import { getFileUrl as getFileUrlFunc } from './getFileUrl';
 
 export class Client extends NotionClient {
   constructor(options: ClientOptions = {}) {
     super(options);
   }
 
-  async getPageProperties(pageId: string, keys: string[] = []) {
-    const res = await this.pages.retrieve({
-      page_id: pageId,
-    });
-
-    if (isPageObjectResponse(res)) {
-      const properties = res.properties;
-
-      // keys가 빈 배열이면 모든 properties를 반환
-      if (keys.length === 0) {
-        return properties;
-      }
-
-      const filteredProperties: Record<string, any> = {};
-
-      // keys 배열을 순회하면서 해당하는 properties 값을 추출
-      keys.forEach((key) => {
-        if (key in properties) {
-          filteredProperties[key] = properties[key];
-        }
-      });
-
-      return filteredProperties; // 추출된 properties 반환
-    }
-
-    return;
-  }
-
-  async getFileUrl(pageId: string, propertyKey: string) {
-    const res = await this.pages.retrieve({
-      page_id: pageId,
-    });
-
-    if (isPropertyFileType(res, propertyKey)) {
-      const property = res.properties[propertyKey]!;
-      const fileObj = property.files[0];
-      if ('file' in fileObj) {
-        const fileUrl = fileObj.file.url;
-        return fileUrl;
-      }
-    }
-    return;
-  }
+  getPageProperties = (pageId: string, keys?: string[]) =>
+    getPagePropertiesFunc(this, pageId, keys);
+  getFileUrl = (pageId: string, propertyKey: string) =>
+    getFileUrlFunc(this, pageId, propertyKey);
 }
 
 export default Client;
