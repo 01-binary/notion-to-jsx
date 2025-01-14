0d895717d6e53a796cd202bea52788a7ec390c95
jinsoo <binary01@kakao.com>
2025-01-14 17:38:10 +0900

    ci: unit test add

--- packages/notion-to-utils/vitest.config.ts
+++ packages/notion-to-utils/vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    environment: 'jsdom',
+    reporters: process.env.GITHUB_ACTIONS
+      ? ['junit', 'default', 'github-actions']
+      : ['junit', 'default'],
+    outputFile: 'test-results.xml',
     coverage: {
       reporter: ['text', 'json-summary', 'json'],
       reportsDirectory: './coverage',
