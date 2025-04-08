b621df9f802bf74123c48d996f97f99d3848c9c4
jinsoo <binary01@kakao.com>
2025-04-08 17:06:29 +0900

    fix: maxWidth change

--- packages/notion-to-jsx/src/components/Renderer/components/Image/styles.css.ts
+++ packages/notion-to-jsx/src/components/Renderer/components/Image/styles.css.ts
@@ -68,11 +68,6 @@ export const styledImage = recipe({
 });
 
 export const placeholder = style({
-  // position: 'absolute',
-  // top: 0,
-  // left: 0,
-  // right: 0,
-  // bottom: 0,
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
--- packages/notion-to-jsx/src/components/Renderer/styles.css.ts
+++ packages/notion-to-jsx/src/components/Renderer/styles.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '../../styles/theme.css';
 
 export const container = style({
-  maxWidth: '900px',
+  maxWidth: '720px',
   margin: '0 auto',
   padding: vars.spacing.xl,
 });
