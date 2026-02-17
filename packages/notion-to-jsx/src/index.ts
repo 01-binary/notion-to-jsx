68204e19f234740e260778b4be7705f36b6561f7
jinsoo <binary01@kakao.com>
2026-02-17 18:13:20 +0900

    test: docs 미변경 v2

--- packages/notion-to-jsx/src/index.ts
+++ packages/notion-to-jsx/src/index.ts
@@ -1,3 +1,4 @@
+// test: vercel ignored build step v2
 export { default as Renderer } from './components/Renderer';
 export type { TocStyleOptions } from './components/Renderer';
 export * from './types';
